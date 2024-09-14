2798ff91cd532edb85103cc95f3a00509f35ec85
Vicente Aloi <aloivicente@gmail.com>
2024-09-14 00:08:41 -0300

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent:() => import('./components/dashboard/dashboard.component'),
+    },
+    {
+        path: 'info',
+        loadComponent:() => import('./components/informacion-easy-turn/informacion-easy-turn.component'),
+    },
+    {
+        path: 'buscarturno',
+        loadComponent:() => import('./components/encontrar-turno/encontrar-turno.component'),
+    }
+];
---
+++ src/app/components/informacion-easy-turn/informacion-easy-turn.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-informacion-easy-turn',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './informacion-easy-turn.component.html',
+  styleUrl: './informacion-easy-turn.component.css'
+})
+export default class InformacionEasyTurnComponent {
+
+}
